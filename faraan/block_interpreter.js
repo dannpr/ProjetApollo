3d46ffa465cbc81ef3456dff697057c09f3e76b3
frozbully <f.rozbully@gmail.com>
2020-11-16 08:25:51 +0100

    block_interpreter.js

--- faraan/block_interpreter.js
+++ faraan/block_interpreter.js
@@ -1,74 +1,52 @@
-//pour éviter tout conflict avec d'autres fichier, toutefonction ou variable globale sera de la forme BI_nom
-/*exemple de blocs :
-    -bloc simples       ["avancer"]
-    -bloc imbricable    ["while",bloc_condition,liste_de_blocs]
-*/
-//addin (seulement sur les bloc imbricables)
-//addafter (soit bloc simples ou soit sur le vide)
+//pour éviter tout conflict avec d'autres fichier, toute fonction ou variable globale sera de la forme BI_nom
 
-//variables globales :
-var BI_blocklist;
-var BI_count;
-BI_reset();//initialise les variables
-
-function BI_newblock(name){
-  /*STR -> int
-    crée un bloc et retourne son identifiant.*/
-  var id = BI_count;
-  BI_count++;
-  // TODO:
-
-  return id;
-}
-function BI_addafter(bloc, id){
-  /*BLOC * int -> int
-    ajoute un bloc après le bloc indiqué
-    retourne l'id du bloc ajouté*/
-    // NOTE: BI_blocklist.splice(n, 0, bloc); insere bloc a la position n
-    // NOTE: attention au imbrications
-    // TODO
-}
-function BI_addin(bloc, id){
-  /*BLOC * int -> int
-    ajoute un bloc **en dernierre position** a l'interieur du bloc indiqué
-    retourne l'id du bloc ajouté*/
-    // NOTE: BI_blocklist.splice(n, 0, bloc); insere bloc a la position n
-    // NOTE: attention au imbrications
-    // TODO:
-
+function BI_query(id) {
+  var buffer = "";
+  var i,start;
+  buffer = document.querySelector(id).innerHTML;
+  i=0;
+  while(i<buffer.length) {
+    if (buffer.slice(i,i+7)=="<button") {
+      start = i;
+      i += 7;
+      while ( buffer[i] != '>') {
+        i++;
+      }
+      buffer = buffer.slice(0,start) + '<' +buffer.slice(i+1);
+      i -= i-start;
+    } else if (buffer.slice(i,i+8)=="</button") {
+      buffer = buffer.slice(0,i)+buffer.slice(i+8);
+      i -= 10;
+    } else {
+      i++;
+    }
+  }
+  return buffer;
 }
 
-function BI_add(name,id){
-  /*STR * int -> int
-    synthèse des 3 fonctions précédentes
-    créé un nouveau bloc
-    détermine automatiquement si il faut utiliser BI_addin ou BI_addafter
-    retourne l'identifiant du bloc créé*/
+function BI_bloctojavascript(buffer) {
+  var res = "";
+  function matchwith (bloc,command) {
+    if (buffer.slice(0,bloc.length) == bloc) {
+      res += command;
+      buffer = buffer.slice(bloc.length);
+      return true;
+    }
+    return false;
+  };
+  while (buffer != "") {
+    if (!(
+      matchwith("<Avancer>", "avancer();") ||
+      matchwith("<Tourner à gauche>", "tourneragauche();") ||
+      matchwith("<Tourner à droite>", "tourneradroite();") ||
+    )){
+      console.error("BI_bloctojavascript ERROR 1 : unknown bloc !");
+      return "";
+    }
+  }
+  return res;
 }
-
-function BI_move(id,newpos){
-  /*int * int -> void
-    change un bloc de place*/
-    // TODO:
-}
-
-function BI_remove(id){
-  /*int -> void
-    supprime un bloc*/
-  // TODO: 
-}
-
-function BI_reset(){
-  /*remet la liste a 0 (quand vide l'écran)*/
-  BI_count=0;
-  BI_blocklist = [];
-}
-
-
 
 function BI_runcode(){
-  /*traduit la liste de bloc en JS et interprette le code obtenu*/
-  // NOTE: doit prendre en compte les différents types de blocs
-  // TODO:
-
+  eval(BI_bloctojavascript(BI_query("#depot")))
 }
