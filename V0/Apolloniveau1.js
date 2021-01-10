2cbc4bc8b7184724ea65431e2079dc28c418abee
Mae-4815162342 <72083970+Mae-4815162342@users.noreply.github.com>
2021-01-10 17:12:28 +0100

    Update Apolloniveau1.js

--- V0/Apolloniveau1.js
+++ V0/Apolloniveau1.js
@@ -162,22 +162,46 @@ function BI_query(id) {
     var i,start;
     buffer = document.querySelector(id).innerHTML;
     i=0;
+		var id="";
     while(i<buffer.length) {
       if (buffer.slice(i,i+7)=="<button") {
         start = i;
         i += 7;
         while ( buffer[i] != '>') {
+					if(buffer.slice(i, i+3)=="id="){
+						var k=i+4;
+						while(buffer[k]!='"'){
+							id=id+buffer[k];
+							k++;
+						}
+					}
           i++;
         }
-        buffer = buffer.slice(0,start) + '<' +buffer.slice(i+1);
+				if(id!=""){
+					console.log(id);
+					buffer = buffer.slice(0,start)+'<'+((document.getElementById(id)).querySelector('input')).value+' '+buffer.slice(i+1);
+				}else{
+        	buffer = buffer.slice(0,start) + '<' +buffer.slice(i+1);
+				}
+				id="";
         i -= i-start;
-      } else if (buffer.slice(i,i+8)=="</button") {
-        buffer = buffer.slice(0,i)+buffer.slice(i+8);
+				//gestion de la balise de Boucle
+
+			}	else if (buffer.slice(i,i+8)=="</button") {
+				buffer = buffer.slice(0,i)+buffer.slice(i+8);
         i -= 10;
-      } else {
-        i++;
-      }
+      } else if (buffer.slice(i, i+6)=="<input") {
+				var j=i+7;
+				while(buffer[j]!=">"){
+					j++;
+				}
+				buffer = buffer.slice(0,i)+buffer.slice(j+1);
+        i -= 10;
+      }else{
+				i++
+			}
     }
+		console.log(buffer);
     return buffer;
   }
   /////////////////////// Partie Block
@@ -191,11 +215,18 @@ function BI_bloctojavascript(buffer) {
       }
       return false;
     };
+		var insert=false;
     while (buffer != "") {
+			for(i=1;i<100; i++){
+				if(matchwith("<"+parseInt(i)+" Boucle>", "for(i=1; i<"+parseInt(i)+"; i++){")){
+					insert=true;
+				}
+			}
       if (!(
 				matchwith("<Avancer>", "avancer(direction);") ||
       matchwith("<Tourner à gauche>", "tourner(direction,\"antihoraire\");") ||
-      matchwith("<Tourner à droite>", "tourner(direction,\"horaire\");")
+      matchwith("<Tourner à droite>", "tourner(direction,\"horaire\");")||
+			matchwith("<Fin de boucle>", "}")||insert==true
       )){
         console.error("BI_bloctojavascript ERROR 1 : unknown bloc !");
         return "";
@@ -205,8 +236,14 @@ function BI_bloctojavascript(buffer) {
   }
 
 function BI_runcode(){
+		if(nbBoucle!=nbFinDeBoucle){
+			alert("Boucle non-fermée!");
+		}
     eval(BI_bloctojavascript(BI_query("#depot")))
   }
+
+
+
 
 
 
@@ -223,6 +260,12 @@ var liste=[];
 //initialisation des coordonnées d'ajout, l'ajout s'effectuant par rapport à la coordonnée gauche supérieure de la zone de dépot:
 var x=5;
 var y=0;
+//compteurs de boucle (permettent de vérifier qu'une boucle est bien fermée, et qu'on essaye pas de fermer une boucle qui n'a jamais été ouverte)
+var nbBoucle=0;
+var nbFinDeBoucle=0;
+//identifiant des balises de boucle pour la récupération des input de l'utilisateur
+var idEntree=1;
+
 
 baliseAll.forEach(balise =>{//ajout d'écouteurs sur les balises du menu, avec les évèments dragStart et dragEnd, marquant respectivement le début et la fin du drag
   balise.addEventListener('dragstart', dragStart);
@@ -280,13 +323,51 @@ function dragDrop(event){
     alert("Il n'y a plus de place!");
   }else{*/
 	const balise= event.dataTransfer.getData("text");//on récupère les données stockées précedement dans "text", permettant de construire une balise identique à celle du menu dans la zone
-	var b=document.createElement("button");//création de la balise
-	b.className='balise depose';//ajout des caractéristiques d'affichage
-	b.textContent=balise;//ajout des données
+	var b;
+	if (balise == "Boucle"){
+		b=document.createElement("button");
+		b.textContent=balise;
+		b.id=parseInt(idEntree);
+		idEntree+=1;
+		var entree=document.createElement("input");
+		entree.type="number";
+		entree.min=1;
+		entree.max=99;
+		entree.title="Entrer un nombre entier positif";
+		entree.value=1;
+		entree.size=2;
+		entree.style.height="20px";
+		b.insertAdjacentElement('beforeend',entree);
+		b.style.height="35px";
+		b.style.top=y+'px';
+		y+=75;
+		b.className='balise depose';
+		nbBoucle+=1;
+	}else{
+		b=document.createElement("button");
+		b.textContent=balise;
+		b.style.top=y+'px';
+		b.className='balise depose';
+		if (balise == "Fin de boucle") {
+			if(nbBoucle>nbFinDeBoucle){
+				nbFinDeBoucle+=1;
+				x-=25;
+				y+=50;
+			}
+			else{
+				alert("Il n'y a pas de balise de boucle à associer à une fin de boucle!");
+				return;
+			}
+		}else{
+			y+=50;
+		}
+	}
+	//ajout des caractéristiques d'affichage
 	//placement de la balise en fonction des coordonnées disponibles
-	b.style.top=y+'px';//coordonnées en x
+//coordonnées en x
 	b.style.left=x+'px';//coordonnées en y
-	y+=50;//on incrémente simplement pour les coordonnées de la prochaine balise déposée
+//on incrémente simplement pour les coordonnées de la prochaine balise déposée
+	if (balise =="Boucle") x+=25;
 	liste.push(b);
 	//la balise est ajoutée à ses coordonnées à la zone de dépot
 	var depot=document.getElementById("depot");
@@ -325,16 +406,47 @@ function suppr_tout(event){//fonction de suppression des balises
 
 function supprimer(event){//fonction de suppression d'une balise unique
   var y1=this.offsetTop;//on récupère l'ordonnée de la balise à supprimer, à partir de laquelle on va pouvoir déterminer quelles balises déplacer pour combler le vide
+	var balise=this.textContent;
   if (bool==1){//si la gomme a été activée, son booléen vaut true(1), on peut donc procéder à la suppression
-  	this.remove();//suppression
-		y-=50;//la coordonnée d'ajout est décrémentée de la place laissée par la balise supprimer, de sorte que les prochaines balises insérées le soit correctement
-  	const balises=document.querySelectorAll('.depose');//on sélectionne toutes les balises restantes dans la zone de dépot
-  	balises.forEach(vide =>{//itération sur chaque balise
-			var k=vide.offsetTop;//on récupère l'ordonnée de la balise
-    	if(k>=y1){//si l'ordonnée de la balise est supérieure à celle de la balise supprimée précédement, il faut la remonter, ce qui est effectué à la ligne suivante en décrémentant l'ordonnée
-      	vide.style.top=(k-50)+'px';
-    	}
-  	});
+		if (balise=="Boucle"){//suppression d'une balise de boucle: tous les éléments contenus dans la boucle sont redécallées vers la gauche jusqu'à la prochaine balise de fin de boucle, qui est aussi supprimée
+			var prochFin=true;
+			nbBoucle-=1;
+			y-=75;
+			this.remove();
+			const balises=document.querySelectorAll('.depose');//on sélectionne toutes les balises restantes dans la zone de dépot
+  		balises.forEach(vide =>{//itération sur chaque balise
+				var k=vide.offsetTop;//on récupère l'ordonnée de la balise
+				var l=vide.offsetLeft;
+    		if(k>=y1){//si l'ordonnée de la balise est supérieure à celle de la balise supprimée précédement, il faut la remonter, ce qui est effectué à la ligne suivante en décrémentant l'ordonnée
+					temp=vide.textContent;
+					if(prochFin){
+						if(temp=="Fin de boucle"){
+							vide.remove();
+							y-=50;
+							prochFin=false;
+							nbFinDeBoucle-=1;
+						}else if(prochFin){
+							vide.style.top=(k-75)+'px';
+							vide.style.left=(l-25)+'px';
+						}
+					}else{
+						vide.style.top=(k-125)+'px';
+					}
+    		}
+  		});
+			if(prochFin) x-=25;//cas où le programme n'a pas rencontré de fin de boucle, auquel cas il faut décrémenter x pour les prochains ajouts
+		}
+		else{
+			y-=50;//la coordonnée d'ajout est décrémentée de la place laissée par la balise supprimer, de sorte que les prochaines balises insérées le soit correctement
+  		this.remove();//suppression
+  		const balises=document.querySelectorAll('.depose');//on sélectionne toutes les balises restantes dans la zone de dépot
+  		balises.forEach(vide =>{//itération sur chaque balise
+				var k=vide.offsetTop;//on récupère l'ordonnée de la balise
+    		if(k>=y1){//si l'ordonnée de la balise est supérieure à celle de la balise supprimée précédement, il faut la remonter, ce qui est effectué à la ligne suivante en décrémentant l'ordonnée
+      		vide.style.top=(k-50)+'px';
+    		}
+  		});
+		}
   }
 }
 
